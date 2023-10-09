5df0bbc4e84840d4b60608b6f6c64dba346d4b95
Dimas0824 <dimassmadapas@gmail.com>
2023-10-09 13:53:54 +0700

    tugas

---
+++ Pertemuan 7/DoWhile20.java
@@ -0,0 +1,10 @@
+public class DoWhile20 {
+    public static void main(String[] args) {
+        int x = 0;
+
+        do{
+            System.out.println(x);
+        }while (++x <= 8);
+        System.out.println("Program berhenti");
+    }
+}
