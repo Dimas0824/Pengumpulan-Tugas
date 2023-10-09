4dbccd2c4d09a4cccb19fd8b214b8ebba85cc58c
Dimas0824 <dimassmadapas@gmail.com>
2023-10-09 11:11:47 +0700

    modif

--- Pertemuan 7/ForKelipatan20.java
+++ Pertemuan 7/ForKelipatan20.java
@@ -8,15 +8,18 @@ public class ForKelipatan20 {
         int kelipatan = input.nextInt();
         int total = 0;
         int counter = 0;
+        double rata = 0;
 
         for (int i = 1; i <= 50; i++) {
             if (i % kelipatan == 0) {
                 total += i;
                 counter++;
+                rata = total / counter;
             }
         }
 
         System.out.println("Banyaknya bilangan " + kelipatan + " dari 1 sampai 50 adalah " + counter);
         System.out.println("Total bilangan kelipatan " + kelipatan + " adalah " + total);
+        System.out.println("Rata-rata bilangan kelipatan " + kelipatan + " adalah " + rata);
     }
 }
