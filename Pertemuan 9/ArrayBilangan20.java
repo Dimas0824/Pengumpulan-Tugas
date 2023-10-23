87c00a62514cefdcd176e7f1e7fb8da865f1c6a5
Dimas0824 <dimassmadapas@gmail.com>
2023-10-23 10:25:09 +0700

    pertanyaan 2 percobaan 1

--- Pertemuan 9/ArrayBilangan20.java
+++ Pertemuan 9/ArrayBilangan20.java
@@ -1,10 +1,6 @@
 public class ArrayBilangan20 {
     public static void main(String[] args) {
-        int[] bil = new int [4];
-        bil[0] = 5;
-        bil[1] = 13;
-        bil[2] = -7;
-        bil[3] = 17;
+        int[] bil = {5, 12867, 7, 2000000};
 
         System.out.println(bil[0]);
         System.out.println(bil[1]);
