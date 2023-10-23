3088d20eda8da4673bd5285320e8e679d693f8c5
Dimas0824 <dimassmadapas@gmail.com>
2023-10-23 11:57:17 +0700

    pertanyaan

--- Pertemuan 9/ArrayRataNilai20.java
+++ Pertemuan 9/ArrayRataNilai20.java
@@ -4,18 +4,31 @@ public class ArrayRataNilai20 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int[] nilaiMhs = new int[10];
-        double total =0;
+        System.out.print("Masukkan jumlah mahasiswa: ");
+        int jumlahMahasiswa = sc.nextInt();
+
+        int[] nilaiMhs = new int[jumlahMahasiswa];
+        double total = 0;
         double rata2;
+        int jumlahLulus = 0;
+        double totalTidakLulus = 0;
 
         for (int i = 0; i < nilaiMhs.length; i++) {
-            System.out.print("Masukkan nilai mahasiswa ke- "+(i+1)+ " : ");
+            System.out.print("Masukkan nilai mahasiswa ke-" + (i + 1) + " : ");
             nilaiMhs[i] = sc.nextInt();
         }
+
         for (int i = 0; i < nilaiMhs.length; i++) {
             total += nilaiMhs[i];
+            if (nilaiMhs[i] > 70) {
+                jumlahLulus++;
+            } else {
+                totalTidakLulus += nilaiMhs[i];
+            }
         }
+
         rata2 = total / nilaiMhs.length;
-        System.out.println("Rata-rata nilai = "+rata2);
+        System.out.println("Rata-rata nilai = " + rata2);
+        System.out.println("Rata-rata nilai tidak lulus = " + (totalTidakLulus / (nilaiMhs.length - jumlahLulus)));
     }
 }
