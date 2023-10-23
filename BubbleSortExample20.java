db4dfc0b6b201aa3396e8b50c2337739e4906e4c
Dimas0824 <dimassmadapas@gmail.com>
2023-10-23 10:13:36 +0700

    Pertemuan 9

---
+++ BubbleSortExample20.java
@@ -0,0 +1,20 @@
+public class BubbleSortExample20 {
+    public static void main(String[] args) {
+        int[] intData = {34 , 18, 87, 72, 32, 54, 43};
+        int temp=0;
+
+        for (int i = 0; i < intData.length; i++) {
+            for (int j = i+1; j < intData.length; j++) {
+                if (intData[i] > intData[j]) {
+                    temp = intData[i];
+                    intData[i] = intData[j];
+                    intData[j] = temp;
+                }
+            }   
+        }
+        System.out.println("Hasil pengurutan: ");
+        for (int i = 0; i < intData.length; i++) {
+            System.out.println(intData[i]);
+        }
+    }
+}
