b1796698d3d942ae5a0d47f24cbbbeb06aa3cd18
Dimas0824 <dimassmadapas@gmail.com>
2023-10-25 10:30:05 +0700

    Modifikasi pertanyaan 1

--- Praktikum 9/LinearSearch20.java
+++ Praktikum 9/LinearSearch20.java
@@ -4,10 +4,21 @@ public class LinearSearch20 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int [] arrayInt = {34, 18, 26, 48, 72, 20, 56, 63};
-        int key= 20;
+        System.out.print("Masukkan jumlah elemen array: ");
+        int jumlahElemen = sc.nextInt();
+
+        int [] arrayInt = new int [jumlahElemen];
+        int key= 0;
         int hasil=0; 
 
+        for (int i = 0; i < arrayInt.length; i++) {
+            System.out.print("Masukkan elemen ke-" + i + ": ");
+            arrayInt[i] = sc.nextInt();
+        }
+        
+        System.out.print("Masukkan key: ");
+        key = sc.nextInt();
+        
         for(int i = 0; i <arrayInt.length; i++) {
             if (arrayInt[i] == key) {
                 hasil = i;
