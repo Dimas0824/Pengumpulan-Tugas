cde75e19ad0195ff588dfb102dabf6b7e8504f2d
Dimas0824 <dimassmadapas@gmail.com>
2023-11-06 14:25:05 +0700

    fixed

--- Praktikum 11/NestedLoop20.java
+++ Praktikum 11/NestedLoop20.java
@@ -2,17 +2,16 @@ import java.util.Scanner;
 
 public class NestedLoop20 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        int[][] temps = new int[2][2];
-        double[] rata = new double[2];
+        Scanner scanner = new Scanner(System.in);
+        double[][] temps = new double[5][7];
+        double[] rata = new double[7];
 
         for (int i = 0; i < temps.length; i++) {
-            System.out.println("Kota ke-" + (i + 1));
-            int total = 0;
-            for (int j = 0; j < temps[i].length; j++) {
-                System.out.print("Hari ke-" + (j + 1) + ": ");
-                temps[i][j] = sc.nextInt();
+            System.out.println("Kota Ke-" + (i + 1));
+            double total = 0;
+            for (int j = 0; j < temps[0].length; j++) {
+                System.out.print("Hari Ke-" + (j + 1) + ": ");
+                temps[i][j] = scanner.nextDouble();
                 total += temps[i][j];
             }
             rata[i] = (double) total / temps[i].length;
@@ -20,13 +19,13 @@ public class NestedLoop20 {
         }
 
         int kota = 1;
-        for (int[] temp : temps) {
-            System.out.print("Kota ke-" + kota + ": ");
-            for (int hari : temp) {
+        for (double[] temp : temps) {
+            System.out.println("Kota Ke-" + kota + " : ");
+            for (double hari : temp) {
                 System.out.print(hari + " ");
             }
             System.out.println();
-            System.out.println("Rata-rata suhu di Kota ke-" + kota + " adalah " + rata[kota - 1]);
+            System.out.println("Rata-rata suhu di Kota ke- " + kota + " adalah " + rata[kota - 1]);
             kota++;
         }
     }
