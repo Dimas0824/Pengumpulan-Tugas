a438fd4c93a6834ffad531914f8a69f343a1e357
Dimas0824 <dimassmadapas@gmail.com>
2023-11-06 14:53:41 +0700

    Tugas Individu

---
+++ Praktikum 11/Individu120.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Individu120 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Masukkan jumlah baris: ");
+        int N = sc.nextInt(); // Jumlah baris
+
+        for (int i = 1; i <= N; i++) {
+            // Membuat spasi sebelum angka
+            for (int j = N - i; j > 0; j--) {
+                System.out.print("  ");
+            }
+
+            // Mencetak angka dari 1 hingga i
+            for (int k = 1; k <= i; k++) {
+                System.out.print(k + " ");
+            }
+
+            System.out.println();
+        }
+    }
+}
