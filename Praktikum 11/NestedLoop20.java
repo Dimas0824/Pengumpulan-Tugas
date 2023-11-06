b8fb80c43e0e854e0967cfc39bcdc22edab42df6
Dimas0824 <dimassmadapas@gmail.com>
2023-11-06 13:50:22 +0700

    pertanyaan 3

--- Praktikum 11/NestedLoop20.java
+++ Praktikum 11/NestedLoop20.java
@@ -5,22 +5,29 @@ public class NestedLoop20 {
         Scanner sc = new Scanner(System.in);
 
         int[][] temps = new int[2][2];
+        double[] rata = new double[2];
 
         for (int i = 0; i < temps.length; i++) {
             System.out.println("Kota ke-" + (i + 1));
+            int total = 0;
             for (int j = 0; j < temps[i].length; j++) {
                 System.out.print("Hari ke-" + (j + 1) + ": ");
                 temps[i][j] = sc.nextInt();
+                total += temps[i][j];
             }
+            rata[i] = (double) total / temps[i].length;
             System.out.println();
         }
 
-        for (int i = 0; i < temps.length; i++) {
-            System.out.print("Kota ke-" + (i + 1) + ": ");
-            for (int j = 0; j < temps[i].length; j++) {
-                System.out.print(temps[i][j] + " ");
+        int kota = 1;
+        for (int[] temp : temps) {
+            System.out.print("Kota ke-" + kota + ": ");
+            for (int hari : temp) {
+                System.out.print(hari + " ");
             }
             System.out.println();
+            System.out.println("Rata-rata suhu di Kota ke-" + kota + " adalah " + rata[kota - 1]);
+            kota++;
         }
     }
 }
