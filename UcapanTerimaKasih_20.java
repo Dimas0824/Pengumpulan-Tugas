bb5237784698948e65f9b7bb08cc270ea623ef68
Dimas0824 <143877889+Dimas0824@users.noreply.github.com>
2023-11-20 12:47:22 +0700

    20-11-2023

--- UcapanTerimaKasih_20.java
+++ UcapanTerimaKasih_20.java
@@ -14,4 +14,8 @@ public class UcapanTerimaKasih_20 {
         System.out.println("Thank you " + nama + " for being the best teacher in the world. \n" +
                 "Your inspired in me a love for learning and made me feel like I could ask you anything");
     }
+
+    public static void main(String[] args) {
+        UcapanTerimaKasih();
+    }
 }
